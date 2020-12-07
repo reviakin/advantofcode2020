15274424a1d6066009036977349a71ff940598f9
reviakin <d.reviakin@8bridge.org>
2020-12-07 20:34:36 +0200

    replace to years fld

--- 2020/day6/part2.js
+++ 2020/day6/part2.js
@@ -13,3 +13,17 @@ document.body.textContent
       )
   )
   .reduce((a, b) => a + b.length, 0);
+
+//
+document.body.textContent
+  .trim()
+  .split("\n\n")
+  .map((code) =>
+    code
+      .split("\n")[0]
+      .split("")
+      .filter((letter) =>
+        code.split("\n").every((part) => part.includes(letter))
+      )
+  )
+  .reduce((a, b) => a + b.length, 0);
